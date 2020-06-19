3885ecf2dd55191516a0b7cd5c0f0bf8561c47f5
perfree <perfree@126.com>
2020-06-19 10:02:25 +0800

    commit

--- src/app/views/portal/home/home.module.ts
+++ src/app/views/portal/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import {NgZorroAntdModule} from 'ng-zorro-antd';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
+    NgZorroAntdModule,
     CommonModule,
     HomeRoutingModule
   ]
