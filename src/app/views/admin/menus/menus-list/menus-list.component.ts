df549b2d94ac5d8fad27d33510820963bfd91763
perfree <perfree@126.com>
2020-06-22 11:54:50 +0800

    commit

--- src/app/views/admin/menus/menus-list/menus-list.component.ts
+++ src/app/views/admin/menus/menus-list/menus-list.component.ts
@@ -1,4 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ComponentFactoryResolver, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {NzMessageService} from 'ng-zorro-antd';
+import {HttpUtil} from '../../../../core/net/httpUtil';
+import {StorageUtil} from '../../../../core/storage/storageUtil';
+
+export interface TreeNodeInterface {
+  id: string;
+  menuName: string;
+  menuIcon: string;
+  menuPath: string;
+  seq: number;
+  target: string;
+  createTime: string;
+  updateTime: string;
+  source: number;
+  flag: number;
+  status: number;
+  type: number;
+  pid: string;
+  level?: number;
+  expand?: boolean;
+  childMenu?: TreeNodeInterface[];
+  parent?: TreeNodeInterface;
+}
 
 @Component({
   selector: 'app-menus-list',
@@ -6,10 +30,103 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menus-list.component.css']
 })
 export class MenusListComponent implements OnInit {
-
-  constructor() { }
+  pageIndex = 1;
+  pageSize = 50;
+  total = 1;
+  loading = true;
+  // 数据集
+  listOfMapData: TreeNodeInterface[] = [];
+  mapOfExpandedData: { [key: string]: TreeNodeInterface[] } = {};
+  validateForm: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private httpUtil: HttpUtil,
+    public storageUtil: StorageUtil,
+    private cfr: ComponentFactoryResolver,
+    private message: NzMessageService
+  ) { }
 
   ngOnInit() {
+    // 初始化表单
+    this.validateForm = this.fb.group({
+      menuName: ''
+    });
+    this.searchData();
   }
 
+  /**
+   * 加载数据
+   */
+  searchData(reset: boolean = false) {
+    if (reset) {
+      this.pageIndex = 1;
+    }
+    const param = {
+      pageIndex: 1,
+      pageSize: 50,
+      form: null,
+    };
+    param.pageIndex = this.pageIndex;
+    param.pageSize = this.pageSize;
+    param.form = this.validateForm.value;
+    this.loading = true;
+    if (param.form.status === null) {
+      param.form.status = 3;
+    }
+    this.httpUtil.post('/menu/list', param).then(res => {
+      console.log(res);
+      this.loading = false;
+      this.total = res.total;
+      this.listOfMapData = res.data;
+      this.listOfMapData.forEach(item => {
+        this.mapOfExpandedData[item.id] = this.convertTreeToList(item);
+      });
+    });
+  }
+
+  collapse(array: TreeNodeInterface[], data: TreeNodeInterface, $event: boolean): void {
+    if ($event === false) {
+      if (data.childMenu) {
+        data.childMenu.forEach(d => {
+          // tslint:disable-next-line:no-non-null-assertion
+          const target = array.find(a => a.id === d.id)!;
+          target.expand = false;
+          this.collapse(array, target, false);
+        });
+      } else {
+        return;
+      }
+    }
+  }
+
+  /**
+   * 转换树形数据
+   * @param root 节点
+   */
+  convertTreeToList(root: TreeNodeInterface): TreeNodeInterface[] {
+    const stack: TreeNodeInterface[] = [];
+    const array: TreeNodeInterface[] = [];
+    const hashMap = {};
+    stack.push({ ...root, level: 0, expand: false });
+    while (stack.length !== 0) {
+      // tslint:disable-next-line:no-non-null-assertion
+      const node = stack.pop()!;
+      this.visitNode(node, hashMap, array);
+      if (node.childMenu && node.childMenu.length > 0) {
+        for (let i = node.childMenu.length - 1; i >= 0; i--) {
+          // tslint:disable-next-line:no-non-null-assertion
+          stack.push({ ...node.childMenu[i], level: node.level! + 1, expand: false, parent: node });
+        }
+      }
+    }
+
+    return array;
+  }
+
+  visitNode(node: TreeNodeInterface, hashMap: { [key: string]: boolean }, array: TreeNodeInterface[]): void {
+    if (!hashMap[node.id]) {
+      hashMap[node.id] = true;
+      array.push(node);
+    }
+  }
 }
--- src/app/views/admin/menus/menus.module.ts
+++ src/app/views/admin/menus/menus.module.ts
@@ -3,13 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { MenusRoutingModule } from './menus-routing.module';
 import { MenusListComponent } from './menus-list/menus-list.component';
+import {NgZorroAntdModule} from 'ng-zorro-antd';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [MenusListComponent],
   imports: [
     CommonModule,
-    MenusRoutingModule
+    NgZorroAntdModule,
+    FormsModule,
+    MenusRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class MenusModule { }
