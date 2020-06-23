66233ae5262d7df75f8994aaffa35a2fc4564e87
perfree <perfree@126.com>
2020-06-23 15:36:58 +0800

    commit

--- src/app/views/admin/category/category-list/category-list.component.ts
+++ src/app/views/admin/category/category-list/category-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ComponentFactoryResolver, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {HttpUtil} from '../../../../core/net/httpUtil';
+import {StorageUtil} from '../../../../core/storage/storageUtil';
+import {NzMessageService} from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-category-list',
@@ -6,10 +10,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-list.component.css']
 })
 export class CategoryListComponent implements OnInit {
-
-  constructor() { }
+  validateForm: FormGroup;
+  // 分页相关
+  pageIndex = 1;
+  pageSize = 50;
+  total = 1;
+  listOfData = [];
+  loading = true;
+  constructor(
+    private fb: FormBuilder,
+    private httpUtil: HttpUtil,
+    public storageUtil: StorageUtil,
+    private cfr: ComponentFactoryResolver,
+    private message: NzMessageService
+  ) { }
 
   ngOnInit() {
+    // 初始化表单
+    this.validateForm = this.fb.group({
+      menuName: '',
+      type: '0'
+    });
+    this.searchData(true);
   }
 
+  /**
+   * 加载数据
+   * @param reset 重置
+   */
+  searchData(reset: boolean = false) {
+    if (reset) {
+      this.pageIndex = 1;
+    }
+    const param = {
+      pageIndex: 1,
+      pageSize: 30,
+      form: null,
+    };
+    param.pageIndex = this.pageIndex;
+    param.pageSize = this.pageSize;
+    param.form = this.validateForm.value;
+    this.loading = true;
+    this.httpUtil.post('/category/list', param).then(res => {
+      console.log(res);
+      this.loading = false;
+      this.total = res.total;
+      this.listOfData = res.data;
+    });
+  }
 }
--- src/app/views/admin/category/category.module.ts
+++ src/app/views/admin/category/category.module.ts
@@ -5,13 +5,18 @@ import { CategoryRoutingModule } from './category-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryUpdateComponent } from './category-update/category-update.component';
 import { CategoryCreateComponent } from './category-create/category-create.component';
+import {NgZorroAntdModule} from 'ng-zorro-antd';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryUpdateComponent, CategoryCreateComponent],
   imports: [
+    NgZorroAntdModule,
+    FormsModule,
     CommonModule,
-    CategoryRoutingModule
+    CategoryRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class CategoryModule { }
