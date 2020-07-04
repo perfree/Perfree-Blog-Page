339e06ce6ee71c4e8d6cdaf0f92b904a040e5ffc
perfree <yin741223257>
2020-07-04 10:16:12 +0800

    :smile: 文章列表

--- src/app/views/admin/article/article.module.ts
+++ src/app/views/admin/article/article.module.ts
@@ -7,10 +7,11 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { ArticleCreateComponent } from './article-create/article-create.component';
 import {SharedModule} from '../../../shared/shared.module';
 import { ArticleCreateSuccessComponent } from './article-create-success/article-create-success.component';
+import { ArticleListComponent } from './article-list/article-list.component';
 
 
 @NgModule({
-  declarations: [ArticleCreateComponent, ArticleCreateSuccessComponent],
+  declarations: [ArticleCreateComponent, ArticleCreateSuccessComponent, ArticleListComponent],
   imports: [
     CommonModule,
     NgZorroAntdModule,
